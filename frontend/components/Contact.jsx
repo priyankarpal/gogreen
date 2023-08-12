f21a76f5ef7d37cd20a5426b830c19e551aad242
Daksh Singh Rathore <dakshrathore1729@gmail.com>
2023-08-12 20:00:46 +0530

    code: imported comp

--- frontend/components/Contact.jsx
+++ frontend/components/Contact.jsx
@@ -1,4 +1,6 @@
 "use client"
+import React from "react";
+import Link from "next/link";
 import { useState } from 'react'
 import { ChevronDownIcon } from '@heroicons/react/20/solid'
 import { Switch } from '@headlessui/react'
