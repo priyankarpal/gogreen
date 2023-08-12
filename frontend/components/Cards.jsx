27e2d20420d2187c941e4c36b990034dd68d8f60
priyankarpal <88102392+priyankarpal@users.noreply.github.com>
2023-08-12 15:43:48 +0530

    fix: change card ui

--- frontend/components/Cards.jsx
+++ frontend/components/Cards.jsx
@@ -1,13 +1,21 @@
+import React from "react";
+import Image from "next/image";
+import { AiOutlineStar } from "react-icons/ai";
+import { MdOutlineArrowForwardIos } from "react-icons/md";
+
 export const Cards = () => {
   return (
     <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
       <div className="grid gap-5 lg:grid-cols-3 sm:max-w-sm sm:mx-auto lg:max-w-full">
         <div className="overflow-hidden rounded-md transition-shadow duration-300 bg-white ">
           <a href="/" aria-label="Article">
-            <img
+            <Image
               src="https://images.pexels.com/photos/932638/pexels-photo-932638.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=3&amp;h=750&amp;w=1260"
-              className="object-cover w-full h-64 rounded"
+              className="object-cover w-full h-64 rounded-md p-2"
               alt=""
+              width={500}
+              height={500}
+
             />
           </a>
           <div className="p-5">
@@ -25,80 +33,41 @@ export const Cards = () => {
               Sed ut perspiciatis unde omnis iste natus error sit sed quia
               consequuntur magni voluptatem doloremque.
             </p>
-            <div className="flex space-x-4">
+            <div className="flex space-x-4 ">
+
               <a
-                href="/"
-                aria-label="Likes"
-                className="flex items-start text-gray-800 transition-colors duration-200 hover:text-deep-purple-accent-700 group"
-              >
-                <div className="mr-2">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="24"
-                    height="24"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    className="w-5 h-5 text-gray-600 transition-colors duration-200 group-hover:text-deep-purple-accent-700"
-                  >
-                    <polyline
-                      points="6 23 1 23 1 12 6 12"
-                      fill="none"
-                      strokeMiterlimit="10"
-                    />
-                    <path
-                      d="M6,12,9,1H9a3,3,0,0,1,3,3v6h7.5a3,3,0,0,1,2.965,3.456l-1.077,7A3,3,0,0,1,18.426,23H6Z"
-                      fill="none"
-                      stroke="currentColor"
-                      strokeMiterlimit="10"
-                    />
-                  </svg>
-                </div>
-                <p className="font-semibold">7.4K</p>
-              </a>
-              <a
-                href="/"
+                href="#"
                 aria-label="Comments"
                 className="flex items-start text-gray-800 transition-colors duration-200 hover:text-deep-purple-accent-700 group"
               >
-                <div className="mr-2">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="24"
-                    height="24"
-                    viewBox="0 0 24 24"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    stroke="currentColor"
-                    className="w-5 h-5 text-gray-600 transition-colors duration-200 group-hover:text-deep-purple-accent-700"
-                  >
-                    <polyline
-                      points="23 5 23 18 19 18 19 22 13 18 12 18"
-                      fill="none"
-                      strokeMiterlimit="10"
-                    />
-                    <polygon
-                      points="19 2 1 2 1 14 5 14 5 19 12 14 19 14 19 2"
-                      fill="none"
-                      stroke="currentColor"
-                      strokeMiterlimit="10"
-                    />
-                  </svg>
+                <div className="mr-2 flex py-1">
+                  <AiOutlineStar />
+                  <AiOutlineStar />
+                  <AiOutlineStar />
+                  <AiOutlineStar />
+                  <AiOutlineStar />
+
+
                 </div>
-                <p className="font-semibold">81</p>
               </a>
+              <p className="flex font-bold ">book now
+
+                <span className="my-1 px-2">
+                  <MdOutlineArrowForwardIos />
+                </span>
+
+              </p>
             </div>
           </div>
         </div>
-        <div className="overflow-hidden transition-shadow duration-300 bg-white rounded-md">
+        <div className="overflow-hidden transition-shadow duration-300  bg-white rounded-md p-2">
           <a href="/" aria-label="Article">
-            <img
+            <Image
               src="https://images.pexels.com/photos/1576937/pexels-photo-1576937.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=2&amp;w=500"
               className="object-cover w-full h-64 rounded"
               alt=""
+              width={500}
+              height={500} AiOutlineStar
             />
           </a>
           <div className="p-5">
@@ -117,79 +86,40 @@ export const Cards = () => {
               consequuntur magni voluptatem doloremque.
             </p>
             <div className="flex space-x-4">
+
               <a
-                href="/"
-                aria-label="Likes"
-                className="flex items-start text-gray-800 transition-colors duration-200 hover:text-deep-purple-accent-700 group"
-              >
-                <div className="mr-2">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="24"
-                    height="24"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    className="w-5 h-5 text-gray-600 transition-colors duration-200 group-hover:text-deep-purple-accent-700"
-                  >
-                    <polyline
-                      points="6 23 1 23 1 12 6 12"
-                      fill="none"
-                      strokeMiterlimit="10"
-                    />
-                    <path
-                      d="M6,12,9,1H9a3,3,0,0,1,3,3v6h7.5a3,3,0,0,1,2.965,3.456l-1.077,7A3,3,0,0,1,18.426,23H6Z"
-                      fill="none"
-                      stroke="currentColor"
-                      strokeMiterlimit="10"
-                    />
-                  </svg>
-                </div>
-                <p className="font-semibold">7.4K</p>
-              </a>
-              <a
-                href="/"
+                href="#"
                 aria-label="Comments"
                 className="flex items-start text-gray-800 transition-colors duration-200 hover:text-deep-purple-accent-700 group"
               >
-                <div className="mr-2">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="24"
-                    height="24"
-                    viewBox="0 0 24 24"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    stroke="currentColor"
-                    className="w-5 h-5 text-gray-600 transition-colors duration-200 group-hover:text-deep-purple-accent-700"
-                  >
-                    <polyline
-                      points="23 5 23 18 19 18 19 22 13 18 12 18"
-                      fill="none"
-                      strokeMiterlimit="10"
-                    />
-                    <polygon
-                      points="19 2 1 2 1 14 5 14 5 19 12 14 19 14 19 2"
-                      fill="none"
-                      stroke="currentColor"
-                      strokeMiterlimit="10"
-                    />
-                  </svg>
+                <div className="mr-2 flex py-1">
+                  <AiOutlineStar />
+                  <AiOutlineStar />
+                  <AiOutlineStar />
+                  <AiOutlineStar />
+                  <AiOutlineStar />
+
+
                 </div>
-                <p className="font-semibold">81</p>
               </a>
+              <p className="flex font-bold ">book now
+
+                <span className="my-1 px-2 ">
+                  <MdOutlineArrowForwardIos />
+                </span>
+
+              </p>
             </div>
           </div>
         </div>
         <div className="overflow-hidden transition-shadow duration-300 bg-white rounded-md">
           <a href="/" aria-label="Article">
-            <img
+            <Image
               src="https://images.pexels.com/photos/2123755/pexels-photo-2123755.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=2&amp;h=750&amp;w=1260"
-              className="object-cover w-full h-64 rounded"
+              className="object-cover w-full h-64 rounded-md p-2"
               alt=""
+              width={500}
+              height={500}
             />
           </a>
           <div className="p-5">
@@ -210,70 +140,29 @@ export const Cards = () => {
               consequuntur magni voluptatem doloremque.
             </p>
             <div className="flex space-x-4">
+
               <a
-                href="/"
-                aria-label="Likes"
-                className="flex items-start text-gray-800 transition-colors duration-200 hover:text-deep-purple-accent-700 group"
-              >
-                <div className="mr-2">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="24"
-                    height="24"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    className="w-5 h-5 text-gray-600 transition-colors duration-200 group-hover:text-deep-purple-accent-700"
-                  >
-                    <polyline
-                      points="6 23 1 23 1 12 6 12"
-                      fill="none"
-                      strokeMiterlimit="10"
-                    />
-                    <path
-                      d="M6,12,9,1H9a3,3,0,0,1,3,3v6h7.5a3,3,0,0,1,2.965,3.456l-1.077,7A3,3,0,0,1,18.426,23H6Z"
-                      fill="none"
-                      stroke="currentColor"
-                      strokeMiterlimit="10"
-                    />
-                  </svg>
-                </div>
-                <p className="font-semibold">7.4K</p>
-              </a>
-              <a
-                href="/"
+                href="#"
                 aria-label="Comments"
                 className="flex items-start text-gray-800 transition-colors duration-200 hover:text-deep-purple-accent-700 group"
               >
-                <div className="mr-2">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="24"
-                    height="24"
-                    viewBox="0 0 24 24"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    stroke="currentColor"
-                    className="w-5 h-5 text-gray-600 transition-colors duration-200 group-hover:text-deep-purple-accent-700"
-                  >
-                    <polyline
-                      points="23 5 23 18 19 18 19 22 13 18 12 18"
-                      fill="none"
-                      strokeMiterlimit="10"
-                    />
-                    <polygon
-                      points="19 2 1 2 1 14 5 14 5 19 12 14 19 14 19 2"
-                      fill="none"
-                      stroke="currentColor"
-                      strokeMiterlimit="10"
-                    />
-                  </svg>
+                <div className="mr-2 flex py-1">
+                  <AiOutlineStar />
+                  <AiOutlineStar />
+                  <AiOutlineStar />
+                  <AiOutlineStar />
+                  <AiOutlineStar />
+
+
                 </div>
-                <p className="font-semibold">81</p>
               </a>
+              <p className="flex font-bold ">book now
+
+                <span className="my-1 px-2">
+                  <MdOutlineArrowForwardIos />
+                </span>
+
+              </p>
             </div>
           </div>
         </div>
