2b95ca149d90b2fd862d1bd0a625bb8b405a2fb0
priyankarpal <88102392+priyankarpal@users.noreply.github.com>
2023-08-13 04:03:35 +0530

    feat: improved sub navbar

--- frontend/components/utility/Subnav.jsx
+++ frontend/components/utility/Subnav.jsx
@@ -1,5 +1,4 @@
-import { AiTwotoneHome } from 'react-icons/ai'
-import { PiCarSimpleDuotone } from 'react-icons/pi'
+import Link from 'next/link';
 
 export const SubNav = () => {
   return (
@@ -7,27 +6,27 @@ export const SubNav = () => {
       <div className="relative flex items-center justify-center  ">
         <ul className="flex items-center justify-center space-x-8 lg:flex">
           <li>
-            <a
+            <Link
               href="/dashboard"
               aria-label="Our product"
               title="Our product"
-              className="font-medium tracking-wide text-black transition-colors duration-200 "
+              className="font-medium tracking-wide text-black transition-colors duration-200 text-[1.5rem] hover:text-purple-500 "
             >
-              <AiTwotoneHome />
-            </a>
+              Hotels
+            </Link>
           </li>
           <li>
-            <a
+            <Link
               href="/"
               aria-label="Our product"
               title="Our product"
-              className="font-medium tracking-wide text-black transition-colors duration-200 "
+              className="font-medium tracking-wide text-black transition-colors duration-200 text-[1.5rem] hover:text-purple-500"
             >
-              <PiCarSimpleDuotone />
-            </a>
+              Cars
+            </Link>
           </li>
         </ul>
       </div>
-    </div>
+    </div >
   );
 };
