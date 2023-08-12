9d64363cf83e1330635553f1490bc8bc138f1974
priyankarpal <88102392+priyankarpal@users.noreply.github.com>
2023-08-12 21:15:12 +0530

    fix: added animation

--- frontend/components/Header.jsx
+++ frontend/components/Header.jsx
@@ -28,7 +28,6 @@ export default function Header() {
   };
   const { user } = useUser();
   const router = useRouter();
-
   const handleButtonClick = () => {
     if (user) {
       router.push('/create'); // Redirect to /create if user exists
@@ -197,39 +196,97 @@ export default function Header() {
       </div>
       <div className="bg-white py-24 sm:py-32">
         <div className="mx-auto max-w-7xl px-6 lg:px-8">
-          <h2 className="text-center text-lg font-semibold leading-8 text-gray-900">
+          <motion.h2
+            initial={{ opacity: 0, scale: 0.5 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{
+              duration: 0.8,
+              delay: 0.3,
+              ease: [0, 0.71, 0.2, 1.01]
+            }}
+            className="text-center text-lg font-semibold leading-8 text-gray-900">
             Trusted by the world’s most prestigious hoteliers
-          </h2>
+          </motion.h2>
           <motion.div className="mx-auto mt-10 grid max-w-lg grid-cols-4 items-center gap-x-8 gap-y-10 sm:max-w-xl sm:grid-cols-6 sm:gap-x-10 lg:mx-0 lg:max-w-none lg:grid-cols-5">
-            <img
+            <motion.img
+              initial={{ opacity: 0, scale: 0.5 }}
+              animate={{ opacity: 1, scale: 1 }}
+              transition={{
+                duration: 0.8,
+                delay: 0.3,
+                ease: [0, 0.71, 0.2, 1.01]
+              }}
+              whileHover={{ scale: 1.2 }}
+              onHoverStart={e => { }}
+              onHoverEnd={e => { }}
               className="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
               src="https://tailwindui.com/img/logos/158x48/transistor-logo-gray-900.svg"
               alt="Transistor"
               width={158}
               height={48}
             />
-            <img
+            <motion.img
+              initial={{ opacity: 0, scale: 0.5 }}
+              animate={{ opacity: 1, scale: 1 }}
+              transition={{
+                duration: 0.8,
+                delay: 0.3,
+                ease: [0, 0.71, 0.2, 1.01]
+              }}
+              whileHover={{ scale: 1.2 }}
+              onHoverStart={e => { }}
+              onHoverEnd={e => { }}
               className="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
               src="https://tailwindui.com/img/logos/158x48/reform-logo-gray-900.svg"
               alt="Reform"
               width={158}
               height={48}
             />
-            <img
+            <motion.img
+              initial={{ opacity: 0, scale: 0.5 }}
+              animate={{ opacity: 1, scale: 1 }}
+              transition={{
+                duration: 0.8,
+                delay: 0.3,
+                ease: [0, 0.71, 0.2, 1.01]
+              }}
+              whileHover={{ scale: 1.2 }}
+              onHoverStart={e => { }}
+              onHoverEnd={e => { }}
               className="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
               src="https://tailwindui.com/img/logos/158x48/tuple-logo-gray-900.svg"
               alt="Tuple"
               width={158}
               height={48}
             />
-            <img
+            <motion.img
+              initial={{ opacity: 0, scale: 0.5 }}
+              animate={{ opacity: 1, scale: 1 }}
+              transition={{
+                duration: 0.8,
+                delay: 0.3,
+                ease: [0, 0.71, 0.2, 1.01]
+              }}
+              whileHover={{ scale: 1.2 }}
+              onHoverStart={e => { }}
+              onHoverEnd={e => { }}
               className="col-span-2 max-h-12 w-full object-contain sm:col-start-2 lg:col-span-1"
               src="https://tailwindui.com/img/logos/158x48/savvycal-logo-gray-900.svg"
               alt="SavvyCal"
               width={158}
               height={48}
             />
-            <img
+            <motion.img
+              initial={{ opacity: 0, scale: 0.5 }}
+              animate={{ opacity: 1, scale: 1 }}
+              transition={{
+                duration: 0.8,
+                delay: 0.3,
+                ease: [0, 0.71, 0.2, 1.01]
+              }}
+              whileHover={{ scale: 1.2 }}
+              onHoverStart={e => { }}
+              onHoverEnd={e => { }}
               className="col-span-2 col-start-2 max-h-12 w-full object-contain sm:col-start-auto lg:col-span-1"
               src="https://tailwindui.com/img/logos/158x48/statamic-logo-gray-900.svg"
               alt="Statamic"
