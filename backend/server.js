254cda9fc33ff94875418ce1525a9d2d5e719dd2
Puskar-Roy <puskarroy600@gmail.com>
2023-08-12 13:34:26 +0530

    add hotels

--- backend/server.js
+++ backend/server.js
@@ -3,13 +3,14 @@ const cors = require("cors");
 const dotenv = require("dotenv");
 const cokkie = require("cookie-parser");
 const app = express();
-
+const router = require("./src/modules/hotels/hotel_routes");
 
 dotenv.config();
 app.use(cors());
 app.use(cokkie());
 app.use(express.json());
-
+app.use('/api',router);
+require('./src/database/connectDb');
 
 
 app.listen(process.env.PORT, () => {
