9be5167d9ac2adb4a6b156e08662a69a3430360d
priyankarpal <88102392+priyankarpal@users.noreply.github.com>
2023-08-12 16:54:04 +0530

    feat: added auth0 func for book now

--- frontend/components/Header.jsx
+++ frontend/components/Header.jsx
@@ -1,9 +1,20 @@
+"use client"
 import React from 'react'
+import { useUser } from '@auth0/nextjs-auth0/client';
+import { useRouter } from 'next/navigation';
 export default function Header() {
+  const { user } = useUser();
+  const router = useRouter();
+
+  const handleButtonClick = () => {
+    if (user) {
+      router.push('/create'); // Redirect to /create if user exists
+    } else {
+      router.push('/api/auth/login'); // Redirect to login if user doesn't exist
+    }
+  };
   return (
     <section>
-
-
       <div className="relative overflow-hidden bg-white">
         <div className="pb-80 pt-16 sm:pb-40 sm:pt-24 lg:pb-48 lg:pt-40">
           <div className="relative mx-auto max-w-7xl px-4 sm:static sm:px-6 lg:px-8">
@@ -83,12 +94,12 @@ export default function Header() {
                   </div>
                 </div>
 
-                <a
-                  href="#"
-                  className="inline-block rounded-md border border-transparent bg-indigo-600 px-8 py-3 text-center font-medium text-white hover:bg-indigo-700"
+                <p onClick={handleButtonClick}
+
+                  className="inline-block rounded-md border border-transparent bg-indigo-600 px-8 py-3 text-center font-medium text-white hover:bg-indigo-700 cursor-pointer"
                 >
-                  Book now
-                </a>
+                  Book Now
+                </p>
               </div>
             </div>
           </div>
