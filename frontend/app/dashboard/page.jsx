4e32f7f46ec400a993881939539229b4d22235ab
priyankarpal <88102392+priyankarpal@users.noreply.github.com>
2023-08-13 00:53:48 +0530

    fix(bug): auth0 problem fixed

--- frontend/app/dashboard/page.jsx
+++ frontend/app/dashboard/page.jsx
@@ -2,18 +2,32 @@
 import { Cards } from "@/components/Cards";
 import Search from "@/components/utility/Search";
 import { SubNav } from "@/components/utility/Subnav";
-// import { useUser } from '@auth0/nextjs-auth0/client';
-// import { useRouter } from 'next/navigation'
-
+import { useUser } from '@auth0/nextjs-auth0/client';
+import { useEffect } from "react";
+import { useRouter } from 'next/navigation'
+import axios from 'axios';
 
 
 export default function Dashboard() {
-    // const router = useRouter()
-    // const { user } = useUser();
-    // console.log(user)
-    // if (!user) {
-    //     router.push("/")
-    // }
+
+    const router = useRouter()
+    const { user } = useUser();
+    console.log(user)
+    useEffect(() => {
+        if (!user) {
+            router.push("/")
+        }
+        else {
+            axios.post('http://localhost:8080/api/users/getUserId', { user })
+                .then(res => {
+                    console.log(res)
+                })
+                .catch(err => {
+                    console.log(err)
+                })
+
+        }
+    }, [user])
 
     return (
         <section className="flex justify-center items-center flex-col">
