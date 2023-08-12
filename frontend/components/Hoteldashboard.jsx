f52a01d19cda931ff151fe0e5674cb7158468ccc
richard-jhavtech <richard.rozario@jhavtech.com>
2023-08-13 04:12:08 +0530

    hotel details update form

--- frontend/components/Hoteldashboard.jsx
+++ frontend/components/Hoteldashboard.jsx
@@ -257,7 +257,8 @@ export default function Hoteldashboard() {
                   name="about"
                   rows={3}
                   className="block resize-none w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                  defaultValue={""}
+                  defaultValu
+                  e={""}
                 />
               </div>
             </div>
@@ -275,7 +276,8 @@ export default function Hoteldashboard() {
                   rows={3}
                   placeholder="enter details "
                   className="block resize-none w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                  defaultValue={""}
+                  defaultValu
+                  e={""}
                 />
               </div>
             </div>
